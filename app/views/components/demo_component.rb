e596d286ac620027690bcb6b6bdcbb25a23f5a25
Peter Berkenbosch <info@peterberkenbosch.nl>
2024-08-17 12:13:47 +0200

    Using TableIcons instead to make this work

--- app/views/components/demo_component.rb
+++ app/views/components/demo_component.rb
@@ -4,6 +4,7 @@ class DemoComponent < ApplicationComponent
   def view_template
     h1 { "Demo" }
     p { "Find me in app/views/components/demo_component.rb" }
-    heroicon "academic-cap", class: "h-6 w-6"
+    # heroicon "academic-cap", class: "h-6 w-6"
+    render Phlex::TablerIcons::Rocket.new(class: "h-12 w-12", stroke_width: 0.5)
   end
 end
--- test/components/demo_component_test.rb
+++ test/components/demo_component_test.rb
@@ -1,13 +1,14 @@
 require "test_helper"
-require "phlex/testing/view_helper"
+require "phlex/testing/capybara"
 
 class DemoComponentTest < ActionView::TestCase
-  include Phlex::Testing::ViewHelper
+  include Phlex::Testing::Capybara::ViewHelper
 
   test "renders the component" do
     result = render DemoComponent.new
 
     assert_selector(result, "h1", text: "Demo")
     assert_selector(result, "p", text: "Find me in app/views/components/demo_component.rb")
+    assert_selector(result, "svg")
   end
 end
