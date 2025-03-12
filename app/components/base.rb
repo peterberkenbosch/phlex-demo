f0b0b88ba0a3d1d48fad0f105ad09a78b3da749f
Peter Berkenbosch <info@peterberkenbosch.nl>
2025-03-12 08:24:35 +0100

    Use Phlex::FIFOCacheStore

--- app/components/base.rb
+++ app/components/base.rb
@@ -6,9 +6,17 @@ class Components::Base < Phlex::HTML
   # Include any helpers you want to be available across all components
   include Phlex::Rails::Helpers::Routes
 
+  CACHE = Phlex::FIFOCacheStore.new(
+    max_bytesize: 20_000_000 # 20MB
+  )
+
   def cache_store
-    Rails.cache
+    CACHE
   end
+
+  # def cache_store
+  #   Rails.cache
+  # end
 
   if Rails.env.development?
     def before_template
